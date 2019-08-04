512d90b7cfc65655f1216a793aaa8c8baaf3c9cf
Sean Hale <seanhale1@live.com>
2019-08-04 12:13:46 -0400

    camera filter

--- Assets/Scripts/FullscreenSprite.cs
+++ Assets/Scripts/FullscreenSprite.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class FullscreenSprite : MonoBehaviour
 {
+
+    public float overflow = 0.2f;
     void Start()
     {
-        float width = FullscreenSprite.GetScreenToWorldWidth;
-        transform.localScale = Vector3.one * width;
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        Vector3 xWidth = transform.localScale;
+        xWidth.x = (worldScreenWidth / width) + overflow;
+        transform.localScale = xWidth;
+        //transform.localScale.x = worldScreenWidth / width;
+        Vector3 yHeight = transform.localScale;
+        yHeight.y = (worldScreenHeight / height) + overflow;
+        transform.localScale = yHeight;
+        //transform.localScale.y = worldScreenHeight / height;
     }
 
     public static float GetScreenToWorldHeight
